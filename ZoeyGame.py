cc2f3a5d806a864d381ad9e1b7bab9476f691fb0
Zoey <zsears639@lsr7.net>
2018-12-14 14:12:13 +0000

    finish the game

--- ZoeyGame.py
+++ ZoeyGame.py
@@ -8,9 +8,10 @@ BLUE = (0, 0, 255)
 Green = (0, 255, 0)
 White = (255, 255, 255)
 
+
 lives = 3
 x = 0
-y = 3
+y = 4
 lives_1 = 0
 lives_2 = 1
 lives_3 = 2
@@ -18,8 +19,8 @@ points = 0
 
 game_over = False
 
-matrix = [[BLUE for column in range(1,8)] for row in range(3,8)]
-draw_life = [[White for cloumn in range (0)]for row in range(0,2)]
+matrix = [[BLUE for column in range(8)] for row in range(8)]
+#draw_life = [[White for cloumn in range (0)]for row in range(0,2)]
 
 def flatten(matrix):
     flattened = [pixel for row in matrix for pixel in row]
@@ -29,7 +30,7 @@ def flatten(matrix):
 def gen_pipes(matrix):
     for row in matrix:
         row[-1] = RED
-    gap = randint(3, 6)
+    gap = randint(1, 6)
     matrix[gap][-1] = BLUE
     matrix[gap - 1][-1] = BLUE
     matrix[gap + 1][-1] = BLUE
@@ -38,7 +39,7 @@ def gen_pipes(matrix):
 
 def move_pipes(matrix):
     for row in matrix:
-        for i in range(3,8):
+        for i in range(7):
             row[i] = row[i + 1]
         row[-1] = BLUE
     return matrix
@@ -60,16 +61,6 @@ def draw_astronaut(event):
     sense.set_pixel(x, y, Green)
     if matrix[y][x] == RED:
       game_over = True
-      
-def draw_lives(draw_life):
-  global x
-  global lives_1
-  global lives_2
-  global lives_3
-  if lives == 3:
-    sense.set_pixel(0,0, (255, 255, 255))
-    sense.set_pixel(0,1, (255, 255, 255))
-    sense.set_pixel(0,2, (255, 255, 255))
 
     
 def check_collision(matrix):
@@ -78,7 +69,6 @@ def check_collision(matrix):
     else:
         return False
     
-
 sense.stick.direction_any = draw_astronaut
 
 while not game_over:
@@ -87,13 +77,9 @@ while not game_over:
         points = points + 1
         if points == 20:
             sense.show_message('You Win', text_colour = Green)
-            points = 0 
-            lives = 3
+            points = points 
+            lives = 1
             game_over = False
-        if lives == 3:
-          sense.set_pixel(0,0, (255, 255, 255))
-          sense.set_pixel(0,1, (255, 255, 255))
-          sense.set_pixel(0,2, (255, 255, 255))
     
     if check_collision(matrix):
         game_over = True
@@ -106,16 +92,14 @@ while not game_over:
         
         if check_collision(matrix):
             lives = lives - 1
+            points = points -1 
         sleep(1)
-        if lives == 2:
-          sense.set_pixel(x,lives_3,BLUE)
-        if lives == 1:
-          sense.set_pixel(x,lives_2,BLUE)
         if lives == 0:
-          sense.set_pixel(x,lives_1,BLUE)
           sleep(1)
           game_over = True
           sleep(1)
+        if points == -1:
+            game_over = True
         while game_over:
               sense.show_message('You Lose',text_colour = RED)
               sense.show_message('Points: ' + str(points), text_colour = BLUE)
